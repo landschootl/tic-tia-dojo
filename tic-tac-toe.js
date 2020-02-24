57cdd01f81aa19fb550d0894c05d442132270721
ludovic landschoot <ludovic.landschoot@ext.leroymerlin.fr>
2020-02-24 13:09:17 +0100

    feat (game): add board class

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,3 +1,6 @@
+/**
+ * Class that represents a player
+ */
 class Player {
     constructor(name, symbol) {
         this.name = name;
@@ -5,6 +8,28 @@ class Player {
     }
 }
 
+/**
+ * Class that represents a game board
+ */
+class Board {
+    cells = [
+        new Cell(0, 0), new Cell(0, 1), new Cell(0, 2),
+        new Cell(1, 0), new Cell(1, 1), new Cell(1, 2),
+        new Cell(2, 0), new Cell(2, 1), new Cell(2, 2)
+    ];
+
+    getCell(posX, posY) {
+        return this.cells.find(cell => cell.posX === posX && cell.posY === posY);
+    }
+
+    draw() {
+        this.cells.forEach(cell => document.getElementById(`cell-${cell.posX}-${cell.posY}`).innerHTML = cell.value);
+    }
+}
+
+/**
+ * Class that represents a cell of game board
+ */
 class Cell {
     constructor(posX, posY) {
         this.posX = posX;
@@ -13,22 +38,17 @@ class Cell {
     }
 }
 
+/**
+ * Class that represents a game
+ */
 class Game {
     round = 0;
     players = [
         new Player('player 1', 'X'),
         new Player('player 2', 'O')
     ];
-    board = [
-        new Cell(0, 0), new Cell(0, 1), new Cell(0, 2),
-        new Cell(1, 0), new Cell(1, 1), new Cell(1, 2),
-        new Cell(2, 0), new Cell(2, 1), new Cell(2, 2)
-    ];
+    board = new Board();
     isFinish = false;
-
-    drawBoard() {
-        this.board.forEach(cell => document.getElementById(`cell-${cell.posX}-${cell.posY}`).innerHTML = cell.value);
-    };
 
     getActualPlayer() {
         return this.players[this.round % this.players.length];
@@ -38,7 +58,7 @@ class Game {
         const actualPlayer = this.getActualPlayer();
         const cellPlay = cell || this.selectCell();
         cellPlay.value = actualPlayer.symbol;
-        this.drawBoard();
+        this.board.draw();
         this.checkVictory(actualPlayer);
         this.round++;
     };
@@ -49,22 +69,20 @@ class Game {
 
     selectCell() {
         /** TO IMPLEMENT */
-        const availableCells = this.board
-            .filter(cell => cell.value === null);
-
+        const availableCells = this.board.cells.filter(cell => cell.value === null);
         return availableCells[Math.floor(Math.random() * Math.floor(availableCells.length))];
     };
 
     checkVictory(actualPlayer) {
         [
-            [this.board[0], this.board[1], this.board[2]],
-            [this.board[3], this.board[4], this.board[5]],
-            [this.board[6], this.board[7], this.board[8]],
-            [this.board[0], this.board[3], this.board[6]],
-            [this.board[1], this.board[4], this.board[7]],
-            [this.board[2], this.board[5], this.board[8]],
-            [this.board[0], this.board[4], this.board[8]],
-            [this.board[2], this.board[4], this.board[6]]
+            [this.board.cells[0], this.board.cells[1], this.board.cells[2]],
+            [this.board.cells[3], this.board.cells[4], this.board.cells[5]],
+            [this.board.cells[6], this.board.cells[7], this.board.cells[8]],
+            [this.board.cells[0], this.board.cells[3], this.board.cells[6]],
+            [this.board.cells[1], this.board.cells[4], this.board.cells[7]],
+            [this.board.cells[2], this.board.cells[5], this.board.cells[8]],
+            [this.board.cells[0], this.board.cells[4], this.board.cells[8]],
+            [this.board.cells[2], this.board.cells[4], this.board.cells[6]]
         ].forEach(winningCombination => {
             if (winningCombination.filter(cell => cell.value === actualPlayer.symbol).length === 3) {
                 this.isFinish = true;
@@ -93,8 +111,8 @@ function main() {
     document.querySelectorAll("#board button").forEach(button => button.addEventListener('click', function () {
         startGameBtn.disabled = true;
         const posCell = this.getAttribute('id').split('-');
-        const checkCell = game.board.find(cell => cell.posX === parseInt(posCell[1]) && cell.posY === parseInt(posCell[2]));
-        if (game.cellIsAvailable(checkCell)) {
+        const checkCell = game.board.getCell(parseInt(posCell[1]),parseInt(posCell[2]));
+        if (!game.isFinish && game.cellIsAvailable(checkCell)) {
             game.nextRound(checkCell);
             if (!game.isFinish) {
                 game.nextRound();
