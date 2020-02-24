d443d5a6b2a8a25fb57f65aac79d9e99412343ce
landchootl <ludovic.landschoot.partner@decathlon.com>
2020-02-24 21:14:06 +0100

    refactor (game): optimize ia

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -58,6 +58,16 @@ class Game {
     ];
     board = new Board();
     isFinish = false;
+    winningCombinations = [
+        [this.board.cells[0], this.board.cells[1], this.board.cells[2]],
+        [this.board.cells[3], this.board.cells[4], this.board.cells[5]],
+        [this.board.cells[6], this.board.cells[7], this.board.cells[8]],
+        [this.board.cells[0], this.board.cells[3], this.board.cells[6]],
+        [this.board.cells[1], this.board.cells[4], this.board.cells[7]],
+        [this.board.cells[2], this.board.cells[5], this.board.cells[8]],
+        [this.board.cells[0], this.board.cells[4], this.board.cells[8]],
+        [this.board.cells[2], this.board.cells[4], this.board.cells[6]]
+    ];
 
     getActualPlayer() {
         return this.players[this.round % this.players.length];
@@ -66,6 +76,7 @@ class Game {
     nextRound(cell) {
         const actualPlayer = this.getActualPlayer();
         const cellPlay = cell || this.selectCell(actualPlayer);
+        console.log('cellPlay', cellPlay);
         cellPlay.value = actualPlayer.symbol;
         this.board.draw(this.players);
         this.checkVictory(actualPlayer);
@@ -84,21 +95,68 @@ class Game {
         this.getActualPlayer(); // the current player
 
         /**** return cell *******/
-        const availableCells = this.board.cells.filter(cell => cell.value === null);
-        return availableCells[Math.floor(Math.random() * Math.floor(availableCells.length))];
+        // Met random sur les 2 premiers tours
+        if (this.round < 2) {
+            const availableCells = this.board.cells.filter(cell => cell.value === null);
+            return availableCells[Math.floor(Math.random() * Math.floor(availableCells.length))];
+        }
+
+        // Met un poid sur chaque combinaisons
+        // 3        =   3 pos win                               x
+        // 2        =   2 pos win et 1 libre
+        // 1        =   1 pos win et 2 libres
+        // -8       =   1 pos enemy et 2 pos win                x
+        // -9       =   1 pos enemy, 1 pos win et 1 libre
+        // -10      =   1 pos enemy et 2 libre
+        // -19      =   2 pos enemy et 1 pos win                x
+        // -20      =   2 pos enemy et 1 libre
+        // -30      =   3 pos enemy                             x
+        const combinations = this.winningCombinations.map(winningCombination => {
+            let weight = 0;
+            winningCombination.forEach(cell => {
+                if (cell.value !== null) {
+                    weight += (cell.value === this.getActualPlayer().symbol ? 1 : -10);
+                }
+            });
+            return {weight: weight, winningCombination: winningCombination}
+        });
+
+        // Regarde les combinaisons encore possible (une case de libre au minimum)
+        const availableCombinations = combinations.filter(enemyCombination => ![3, -8, -19, -30].includes(enemyCombination.weight));
+
+        // Si il y a une possibilité de gagner avec 2 win et 1 libre
+        const winCombinationPonderous = availableCombinations.find(enemyCombination => enemyCombination.weight === 2);
+        if (winCombinationPonderous !== undefined) {
+            return winCombinationPonderous.winningCombination.find(cell => cell.value === null);
+        }
+
+        // Si il y a un risque 2 enemy et 1 libre
+        const enemyCombinations = availableCombinations.find(enemyCombination => enemyCombination.weight === -20);
+        if (enemyCombinations !== undefined) {
+            return enemyCombinations.winningCombination.find(cell => cell.value === null);
+        }
+
+        // Si il y a deux cases libre et 1 case win
+        let nextCombination = availableCombinations.find(enemyCombination => enemyCombination.weight === 1);
+        if (nextCombination !== undefined) {
+            return nextCombination.winningCombination.find(cell => cell.value === null);
+        }
+
+        // Si il y a deux cases libre et 1 case win
+        nextCombination = availableCombinations.find(enemyCombination => enemyCombination.weight === -10);
+        if (nextCombination !== undefined) {
+            return nextCombination.winningCombination.find(cell => cell.value === null);
+        }
+
+        // sinon au hasard
+        if (this.round < 2) {
+            const availableCells = this.board.cells.filter(cell => cell.value === null);
+            return availableCells[Math.floor(Math.random() * Math.floor(availableCells.length))];
+        }
     };
 
     checkVictory(actualPlayer) {
-        [
-            [this.board.cells[0], this.board.cells[1], this.board.cells[2]],
-            [this.board.cells[3], this.board.cells[4], this.board.cells[5]],
-            [this.board.cells[6], this.board.cells[7], this.board.cells[8]],
-            [this.board.cells[0], this.board.cells[3], this.board.cells[6]],
-            [this.board.cells[1], this.board.cells[4], this.board.cells[7]],
-            [this.board.cells[2], this.board.cells[5], this.board.cells[8]],
-            [this.board.cells[0], this.board.cells[4], this.board.cells[8]],
-            [this.board.cells[2], this.board.cells[4], this.board.cells[6]]
-        ].forEach(winningCombination => {
+        this.winningCombinations.forEach(winningCombination => {
             if (winningCombination.filter(cell => cell.value === actualPlayer.symbol).length === 3) {
                 this.isFinish = true;
                 winningCombination.forEach(cell => document.getElementById(`cell-${cell.posX}-${cell.posY}`).style.backgroundColor = '#00D68F');
